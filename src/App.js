475303284e4e1b08ca8aca27867dc8570491135e
yunz <maximum_2@icloud.com>
2021-03-18 23:10:24 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import "./styles.css";
+import React, { useState } from "react";
+
+export default function App() {
+  const [item, setItem] = useState(1);
+  const incrementItem = () => setItem(item + 1);
+  const decrementItem = () => setItem(item - 1);
+  return (
+    <div className="App">
+      <h1>Hello {item}</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <button onClick={incrementItem}>Increment</button>
+      <button onClick={decrementItem}>decrement</button>
+    </div>
+  );
+}
+
+class AppUgly extends React.Component {
+  state = {
+    item: 1
+  };
+  render() {
+    const { item } = this.state;
+    return (
+      <div className="App">
+        <h1>Hello {item}</h1>
+        <h2>Start editing to see some magic happen!</h2>
+        <button onClick={this.incrementItem}>Increment</button>
+        <button onClick={this.decrementItem}>decrement</button>
+      </div>
+    );
+  }
+  incrementItem = () => {
+    this.setState((state) => {
+      return {
+        item: state.item + 1
+      };
+    });
+  };
+  decrementItem = () => {
+    this.setState((state) => {
+      return {
+        item: state.item - 1
+      };
+    });
+  };
+}
